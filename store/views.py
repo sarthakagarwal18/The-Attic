551dea72f080afb41a5d54c1e4acddf441bb4b70
Sarthak Agarwal <sarthak18696@gmail.com>
2016-06-07 02:50:58 +0530

    Refactored basetemplate, included background image

--- store/views.py
+++ store/views.py
@@ -12,4 +12,4 @@ def store(request):
     request.session['location'] = "unknown"
     if request.user.is_authenticated():
         request.session['location'] = "Earth"
-    return render(request, 'store.html', context)
+    return render(request, 'base.html', context)
