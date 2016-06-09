c2e79f0c14bf4cbf07c91215f3d250e2bce6fa71
Sarthak Agarwal <sarthak18696@gmail.com>
2016-06-10 00:10:57 +0530

    Add Review Form

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, Author, BookOrder, Cart
+from .models import Book, Author, BookOrder, Cart, Review
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'price', 'stock')
@@ -16,7 +16,11 @@ class BookOrderAdmin(admin.ModelAdmin):
 class CartAdmin(admin.ModelAdmin):
     list_display = ('user', 'active', 'order_date')
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user', 'publish_date', 'text')
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Cart, CartAdmin)
 admin.site.register(BookOrder, BookOrderAdmin)
+admin.site.register(Review, ReviewAdmin)
--- store/views.py
+++ store/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import Book, Cart, BookOrder
+from .models import Book, Cart, BookOrder, Review
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
+from .forms import ReviewForm
 
 def index(request):
     return render(request, 'template.html')
@@ -16,9 +17,25 @@ def store(request):
 
 
 def book_details(request, book_id):
+    book = Book.objects.get(pk=book_id)
     context={
-        'book': Book.objects.get(pk=book_id),
+        'book': book,
     }
+    if request.user.is_authenticated():
+        if request.method == "POST":
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                new_review = Review.objects.create(
+                    user = request.user,
+                    book = context['book'],
+                    text = form.cleaned_data.get('text')
+                )
+                new_review.save()
+        else:
+            if Review.objects.filter(user=request.user, book=context['book']).count() == 0:
+                form = ReviewForm()
+                context['form'] = form
+    context['reviews'] = book.review_set.all()
     return render(request, 'store/detail.html', context)
 
 def add_to_cart(request, book_id):
